96f426ad40b971c7d8896c82b04c0a94da8c132d
Lars Larsen <lal@jyskebank.dk>
2019-03-05 23:07:14 +0100

    added user registration view

---
+++ web-back-tictactoe.web/Controllers/UserRegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web_back_tictactoe.web.Controllers
+{
+    public class UserRegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ web-back-tictactoe.web/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace web_back_tictactoe.web.Models
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        [Required] public string FirstName { get; set; }
+        [Required] public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool IsEmailConfirmed { get; set; }
+        public DateTime? EmailConfirmationDate { get; set; }
+        public int Score { get; set; }
+    }
+}
