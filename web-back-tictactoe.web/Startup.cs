98e988d0a9cc369d6f973d7faf5db65cfda0adcc
lars@k7c.dk <lars@k7c.dk>
2019-03-06 18:55:49 +0100

    added dedicated route for user registration

--- web-back-tictactoe.web/Startup.cs
+++ web-back-tictactoe.web/Startup.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using web_back_tictactoe.web.Extensions;
+using web_back_tictactoe.web.Models;
 using web_back_tictactoe.web.Services;
 
 namespace web_back_tictactoe.web
@@ -13,6 +16,7 @@ namespace web_back_tictactoe.web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddRouting();
 
             services.AddSingleton<IUserService, UserService>();
         }
@@ -29,12 +33,31 @@ namespace web_back_tictactoe.web
 
             app.UseCommunicationMiddleware();
 
+            // Call UserRegistration with dedicated route
+            // http://localhost:5000/createuser?firstname=Lars&lastName=Larsen&email=lars@k7c.dk&password=123
+            var routeBuilder = new RouteBuilder(app);
+            routeBuilder.MapGet("CreateUser", context =>
+            {
+                var firstName = context.Request.Query["firstName"];
+                var lastName = context.Request.Query["lastName"];
+                var email = context.Request.Query["email"];
+                var password = context.Request.Query["password"];
+                var userService = context.RequestServices.GetService<IUserService>();
+                userService.RegisterUser(new UserModel
+                    {FirstName = firstName, LastName = lastName, Email = email, Password = password});
+                return context.Response.WriteAsync($"User {firstName} {lastName} has been sucessfully created.");
+            });
+            var newUserRoutes = routeBuilder.Build();
+            app.UseRouter(newUserRoutes);
+            
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     "default",
                     "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseStatusCodePages("text/plain", "HTTP Error - Status Code: {0}");
         }
     }
 }
