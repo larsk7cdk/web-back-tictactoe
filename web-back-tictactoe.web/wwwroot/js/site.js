d6f57b17794e6f182015203f01a72e80e91ea201
lars@k7c.dk <lars@k7c.dk>
2019-03-09 15:54:00 +0100

    added minify and bundling

--- web-back-tictactoe.web/wwwroot/js/site.js
+++ web-back-tictactoe.web/wwwroot/js/site.js
@@ -1,1 +1,22 @@
-﻿
+var interval;
+
+function EmailConfirmation(email) {
+
+    interval = setInterval(() => {
+        CheckEmailConfirmationStatus(email);
+    },
+        1000);
+}
+function CheckEmailConfirmationStatus(email) {
+    $.get("/CheckEmailConfirmationStatus?email=" + email,
+        function (data) {
+            if (data === "OK") {
+                if (interval !== null) {
+                    clearInterval();
+                    window.location.href = "/GameInvitation?email=" + email;
+                }
+            } else {
+                console.log(data);
+            }
+        });
+}
